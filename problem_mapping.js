bd1a247e6484ffc9db9f3b13667e43e1d8e5e617
Luca Cesarano <lu.cesarano@reply.it>
2022-06-01 09:42:31 +0200

    New solutions

--- problem_mapping.js
+++ problem_mapping.js
@@ -1206,7 +1206,7 @@ problem_mapping = {
                 "total": 10
             },
             "Python: tree_right_sibling.py": {
-                "passed": 0,
+                "passed": 10,
                 "total": 10
             }
         }
